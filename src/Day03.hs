db7e5509a024f9acf1d22ce35dee985acc43779d
Cyryl Waśkiewicz <cyryl.was@gmail.com>
2022-12-03 16:58:17 +0100

    Use same inputtype for part2

--- src/Day03.hs
+++ src/Day03.hs
@@ -2,7 +2,7 @@ module Day03 (solve) where
 import           Data.Char       (isLetter, isLower)
 import           Data.Functor    ((<&>))
 import           Data.List.Split (chunksOf)
-import           Data.Set        (Set, fromList, intersection, toList)
+import           Data.Set        (Set, fromList, intersection, toList, union)
 import           Utils           (pairMap)
 
 verify :: String -> IO String
@@ -14,12 +14,9 @@ split :: [a] -> ([a], [a])
 split xs = splitAt n xs
     where n = length xs `div` 2
 
-parse1 :: String -> IO [(Set Char, Set Char)]
-parse1 = mapM parseLine . lines
+parse :: String -> IO [(Set Char, Set Char)]
+parse = mapM parseLine . lines
     where parseLine line = verify line <&> split <&> pairMap fromList
-
-parse2 :: String -> IO [[Set Char]]
-parse2 s = fmap (chunksOf 3) (mapM (fmap fromList . verify) . lines $ s)
 
 itemPriority :: Char -> Int
 itemPriority c = if isLower c
@@ -29,11 +26,11 @@ itemPriority c = if isLower c
 part1 :: [(Set Char, Set Char)] -> Int
 part1 = sum . map (itemPriority . head . toList . uncurry intersection)
 
-part2 :: [[Set Char]] -> Int
-part2 = sum . map (itemPriority . head . toList . foldl1 intersection)
+part2 :: [(Set Char, Set Char)] -> Int
+part2 = sum . map (itemPriority . head . toList . foldl1 intersection) . chunksOf 3 . map (uncurry union)
 
 solve :: String -> IO (Int, Int)
 solve input = do
-    p1 <- parse1 input <&> part1
-    p2 <- parse2 input <&> part2
+    p1 <- parse input <&> part1
+    p2 <- parse input <&> part2
     return (p1, p2)

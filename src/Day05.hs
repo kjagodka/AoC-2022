ad4ffe9d9f5e8054612d04e7c2f5a6c0136e0175
Cyryl Waśkiewicz <cyryl.was@gmail.com>
2022-12-05 16:54:45 +0100

    Improved parsing

--- src/Day05.hs
+++ src/Day05.hs
@@ -3,9 +3,8 @@ module Day05 (solve) where
 import Control.Monad (foldM)
 import Data.Char (isLetter)
 import Data.Functor ((<&>))
-import Data.List.Split (chunksOf, splitOn)
-import Data.Map as Map (Map, adjust, alter, empty, findWithDefault, foldr, insertWith)
-import Data.Maybe (fromMaybe)
+import Data.List.Split (chunksOf, splitOn, wordsBy)
+import Data.Map as Map (Map, adjust, empty, findWithDefault, foldr, insert, member, size)
 import Utils (applyTuple, joinPair, parseInt)
 
 data Crate = Empty | Crate Char
@@ -19,22 +18,34 @@ type Move = (Int, Int, Int) --(count, from, to)
 
 parse :: String -> IO (Cargo, [Move])
 parse s = case splitOn [""] . lines $ s of
-  [cargoPart, movesPart] -> joinPair (parseCargo cargoPart, mapM parseMove movesPart)
+  [cargoPart, movesPart] -> do
+    cargo <- parseCargo cargoPart
+    moves <- mapM (parseMove cargo) movesPart
+    return (cargo, moves)
   _ -> fail $ "Could not parse input:\n" ++ s
   where
     parseCargo :: [String] -> IO Cargo
-    parseCargo lns = do
-      rows <- mapM parseRow . tail $ reverse lns
-      return $ foldl addRow empty rows
+    parseCargo lns = case reverse lns of
+      [] -> fail "Unable to parsy empty cargo description"
+      legend : rows -> do
+        cargo <- parseLegend legend
+        mapM (parseRow cargo) rows <&> foldl addRow cargo
 
-    parseRow :: String -> IO [Crate]
-    parseRow str = mapM parseCrate . chunksOf 4 $ str ++ " "
+    parseLegend :: String -> IO Cargo
+    parseLegend str = mapM parseInt (wordsBy (== ' ') str) <&> foldl (\cargo key -> insert key [] cargo) empty
+
+    parseRow :: Cargo -> String -> IO [Crate]
+    parseRow cargo str = (mapM parseCrate . chunksOf 4 $ str ++ " ") >>= verifyRow
+      where
+        verifyRow crates =
+          if length crates == size cargo
+            then return crates
+            else fail $ "Could not parse row of cargo: " ++ str ++ "\n. Expected size: " ++ show (size cargo) ++ "Actual size: " ++ show (length crates)
 
     addRow :: Cargo -> [Crate] -> Cargo
     addRow cargo row =
       let numbered = filter ((/= Empty) . snd) . zip [1 ..] $ row
-          addCrate cr = alter (Just . (cr :) . fromMaybe [])
-       in foldl (\cargo' (key, crate) -> addCrate crate key cargo') cargo numbered
+       in foldl (\cargo' (key, crate) -> adjust (crate :) key cargo') cargo numbered
 
     parseCrate :: String -> IO Crate
     parseCrate "    " = return Empty
@@ -44,48 +55,45 @@ parse s = case splitOn [""] . lines $ s of
         else fail $ "Could not parse crate: " ++ cr
     parseCrate cr = fail $ "Could not parse crate: " ++ cr
 
-    parseMove :: String -> IO Move
-    parseMove mv = case splitOn [' '] mv of
+    parseMove :: Cargo -> String -> IO Move
+    parseMove cargo mv = case splitOn [' '] mv of
       ["move", a, "from", b, "to", c] -> do
         a' <- parseInt a
         b' <- parseInt b
         c' <- parseInt c
-        if all (>= 0) [a', b', c']
+        if a' > 0 && member b' cargo && member c' cargo
           then return (a', b', c')
           else fail $ "Encountered negative integer, when parsing move: " ++ mv
       _ -> fail $ "Could not parse move: " ++ mv
 
 makeMove1 :: (MonadFail m) => Cargo -> Move -> m Cargo
-makeMove1 cargo move@(count, from, to) = takeFrom >>= dropAt
+makeMove1 cargo move@(count, from, to) = takeFrom <&> dropAt
   where
     takeFrom =
       let orig = findWithDefault [] from cargo
        in if (< count) . length $ orig
             then fail $ "Not enough crates in stack to make move" ++ show move
             else return (adjust (drop count) from cargo, take count orig)
-    dropAt (cargo', stack) = return $ insertWith (++) to (reverse stack) cargo'
+    dropAt (cargo', stack) = adjust (reverse stack ++) to cargo'
 
 makeMove2 :: (MonadFail m) => Cargo -> Move -> m Cargo
-makeMove2 cargo move@(count, from, to) = takeFrom >>= dropAt
+makeMove2 cargo move@(count, from, to) = takeFrom <&> dropAt
   where
     takeFrom =
       let orig = findWithDefault [] from cargo
        in if (< count) . length $ orig
             then fail $ "Not enough crates in stack to make move" ++ show move
             else return (adjust (drop count) from cargo, take count orig)
-    dropAt (cargo', stack) = return $ insertWith (++) to stack cargo'
+    dropAt (cargo', stack) = adjust (stack ++) to cargo'
 
 crateToChar :: MonadFail m => Crate -> m Char
 crateToChar Empty = fail "Tried to show empty"
 crateToChar (Crate c) = return c
 
-
 solvePart :: (MonadFail m) => (Cargo -> Move -> m Cargo) -> (Cargo, [Move]) -> m String
 solvePart f (cargo, moves) =
   foldM f cargo moves
-    <&> Map.foldr (\stack acc -> head stack : acc) []
-    >>= mapM crateToChar
-
+    >>= mapM crateToChar . Map.foldr (\stack acc -> head stack : acc) []
 
 part1 :: (Cargo, [Move]) -> IO String
 part1 = solvePart makeMove1
@@ -94,4 +102,4 @@ part2 :: (Cargo, [Move]) -> IO String
 part2 = solvePart makeMove2
 
 solve :: String -> IO (String, String)
-solve input = parse input <&> applyTuple (part1, part2) >>= joinPair
+solve input = parse input >>= joinPair . applyTuple (part1, part2)

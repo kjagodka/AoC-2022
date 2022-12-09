85258835c20ab204743c01a70478bd5420952aa8
Cyryl Waśkiewicz <cyryl.was@gmail.com>
2022-12-09 17:23:46 +0100

    Solved part2 of day9

--- src/Day09.hs
+++ src/Day09.hs
@@ -6,9 +6,9 @@ import Utils (applyTuple, pairMap, parseInt)
 
 data Move = MUp | MDown | MLeft | MRight
 
-type RopeEnd = (Int, Int)
+type Knot = (Int, Int)
 
-type Rope = (RopeEnd, RopeEnd) --(head, tail)
+type Rope = [Knot] --(head, tail)
 
 parse :: String -> IO [Move]
 parse = fmap concat . mapM parseLine . lines
@@ -27,37 +27,43 @@ parse = fmap concat . mapM parseLine . lines
     parseMove mv = fail $ "Could not parse move: " ++ mv
 
 applyMove :: Rope -> Move -> Rope
-applyMove (h, t) m =
-  let h' = moveHead h m
-      t' = moveTail t h'
-   in (h', t')
+applyMove (h:t) m = result
   where
-    moveHead :: RopeEnd -> Move -> RopeEnd
+    result = moveHead h m : zipWith follow t result
+
+    moveHead :: Knot -> Move -> Knot
     moveHead (hx, hy) MUp = (hx, hy + 1)
     moveHead (hx, hy) MDown = (hx, hy - 1)
     moveHead (hx, hy) MLeft = (hx - 1, hy)
     moveHead (hx, hy) MRight = (hx + 1, hy)
 
-    moveTail :: RopeEnd -> RopeEnd -> RopeEnd
-    moveTail (tx, ty) (hx, hy)
+    follow :: Knot -> Knot -> Knot
+    follow (tx, ty) (hx, hy)
       | areTouching (tx, ty) (hx, hy) = (tx, ty)
       | tx == hx = (tx, towards ty hy)
       | ty == hy = (towards tx hx, ty)
       | otherwise = (towards tx hx, towards ty hy)
 
-    areTouching :: RopeEnd -> RopeEnd -> Bool
+    areTouching :: Knot -> Knot -> Bool
     areTouching (tx, ty) (hx, hy) = max (abs (hx - tx)) (abs (hy - ty)) < 2
 
     towards :: Int -> Int -> Int
     towards from to = from + signum (to - from)
+applyMove [] _ = []
 
-ropePositions :: [Move] -> [Rope]
-ropePositions moves = positions
+simulateRope :: Rope -> [Move] -> [Rope]
+simulateRope initial moves = positions
   where
-    positions = ((0, 0), (0, 0)) : zipWith applyMove positions moves
+    positions = initial : zipWith applyMove positions moves
+
+countTailPositions :: [Rope] -> Int
+countTailPositions = size . fromList . map last
 
 part1 :: [Move] -> Int
-part1 = size . fromList . map snd . ropePositions
+part1 = countTailPositions . simulateRope (replicate 2 (0, 0))
+
+part2 :: [Move] -> Int
+part2 = countTailPositions . simulateRope (replicate 10 (0, 0))
 
 solve :: String -> IO (String, String)
-solve input = parse input <&> applyTuple (part1, part1) <&> pairMap show
+solve input = parse input <&> applyTuple (part1, part2) <&> pairMap show

2e18f747b6a8532cfef62aca929983875043f16e
Cyryl Waśkiewicz <cyryl.was@gmail.com>
2022-12-13 20:58:48 +0100

    Renamed data Packet to Value

--- src/Day13.hs
+++ src/Day13.hs
@@ -6,41 +6,41 @@ import Text.Read (readMaybe)
 import Utils (applyTuple, pairMap)
 import Data.Bifunctor (first)
 
-data Packet = Number Int | List [Packet]
+data Value = Number Int | List [Value]
 
-instance Read Packet where
+instance Read Value where
   readsPrec precedence str
     | null str = []
     | isDigit $ head str = map (first Number) $ readsPrec precedence str
     | (== '[') $ head str = map (first List) $ readsPrec precedence str
     | otherwise = []
 
-instance Ord Packet where
+instance Ord Value where
   (Number n) `compare` (Number m) = n `compare` m
   (Number n) `compare` (List l) = [Number n] `compare` l
   (List l) `compare` (Number n) = l `compare` [Number n]
   (List l1) `compare` (List l2) = l1 `compare` l2
 
-instance Eq Packet where
+instance Eq Value where
   a == b = (a `compare` b) == EQ
 
-parse :: MonadFail m => String -> m [(Packet, Packet)]
+parse :: MonadFail m => String -> m [(Value, Value)]
 parse = mapM parsePair . splitOn [""] . lines
   where
-    parsePair :: MonadFail m => [String] -> m (Packet, Packet)
+    parsePair :: MonadFail m => [String] -> m (Value, Value)
     parsePair [a, b] =
-      let a' = readMaybe a :: Maybe Packet
-          b' = readMaybe b :: Maybe Packet
+      let a' = readMaybe a :: Maybe Value
+          b' = readMaybe b :: Maybe Value
        in case (a', b') of
             (Just a'', Just b'') -> return (a'', b'')
             (Nothing, _) -> fail $ "Could not parse packet: " ++ a
             (_, Nothing) -> fail $ "Could not parse packet: " ++ b
     parsePair lns = fail $ "Wrong number of lines, expected pair of packets in:\n" ++ unlines lns
 
-part1 :: [(Packet, Packet)] -> Int
+part1 :: [(Value, Value)] -> Int
 part1 = sum . map fst . filter (uncurry (<=) . snd) . zip [1 ..]
 
-part2 :: [(Packet, Packet)] -> Int
+part2 :: [(Value, Value)] -> Int
 part2 pairs =
   let packets = concatMap (\(a, b) -> [a, b]) $ (read "[[2]]", read "[[6]]") : pairs
       i1 = length . filter (<= read "[[2]]") $ packets
